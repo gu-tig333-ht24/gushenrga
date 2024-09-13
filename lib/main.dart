abbe5b2feed84ce96a9fe2eac41f2e37292f7ac8
gushenrga <gushenrga@student.gu.se>
2024-09-13 14:30:16 +0200

    Steg1

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,157 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TodoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+// Huvudklass
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TIG333 Att Göra',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.grey, // Grå färgtema för appen
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TodoListScreen(), // Startskärmen är TodoListScreen
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// Skärm som visar att göra-listan
+class TodoListScreen extends StatelessWidget {
+  // Statisk lista över att göra-uppgifter
+  final List<String> todos = [
+    "Läsa en bok",
+    "Göra inlämning",
+    "Städa",
+    "Gym",
+    "Powernap",
+    "Handla",
+    "Ha kul",
+    "Gibb"
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white, // Bakgrundsfärgen på skärmen sätts till vit
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.indigo[900], // Mörkblå bakgrundsfärg för AppBar
+        title: Text('TIG333 Att Göra'), // Titel på appbaren
+        actions: [
+          // Popupmeny för att välja filtrering
+          PopupMenuButton<String>(
+            onSelected: (String result) {
+              // Här kan filtreringslogik implementeras
+            },
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+              const PopupMenuItem<String>(
+                value: 'all',
+                child: Text('Alla'), // Val för att visa alla uppgifter
+              ),
+              const PopupMenuItem<String>(
+                value: 'done',
+                child: Text('Gjorda'), // Val för att visa bara gjorda uppgifter
+              ),
+              const PopupMenuItem<String>(
+                value: 'undone',
+                child: Text('Ogjorda'), // Val för att visa bara ogjorda uppgifter
+              ),
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      // Lista över uppgifter
+      body: ListView.builder(
+        itemCount: todos.length, // Antal uppgifter i listan
+        itemBuilder: (context, index) {
+          // Skapar en TodoItem för varje uppgift
+          return TodoItem(todo: todos[index], isDone: index == 2); 
+          // Markera "Städa" som gjord (index 2)
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // När man trycker på + knappen öppnas en ny skärm för att lägga till uppgift
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddTodoScreen()),
+          );
+        },
+        child: Icon(Icons.add), // Plus-ikon för att lägga till nya uppgifter
+      ),
+    );
+  }
+}
+
+// Visuellt element för en enskild att göra-uppgift
+class TodoItem extends StatelessWidget {
+  final String todo; // Namnet på uppgiften
+  final bool isDone; // Om uppgiften är klar eller inte
+
+  TodoItem({required this.todo, this.isDone = false}); // Konstruktor
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Checkbox(
+        value: isDone, // Checkbox för att markera om uppgiften är klar
+        onChanged: (bool? value) {
+          // Här kan logik för att markera som klar implementeras
+        },
+      ),
+      title: Text(
+        todo, // Visar uppgiftens namn
+        style: TextStyle(
+          decoration: isDone ? TextDecoration.lineThrough : TextDecoration.none,
+          // Stryker över text om uppgiften är klar
+        ),
+      ),
+      trailing: IconButton(
+        icon: Icon(Icons.delete), // Soptunneikon för att ta bort uppgiften
+        onPressed: () {
+          // Här kan logik för att ta bort uppgift implementeras
+        },
+      ),
+    );
+  }
+}
+
+// Skärm för att lägga till nya uppgifter
+class AddTodoScreen extends StatelessWidget {
+  final TextEditingController textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Sätter bakgrundsfärgen till vit även här
+      appBar: AppBar(
+        backgroundColor: Colors.indigo[900], // Mörkblå bakgrundsfärg för AppBar
+        leading: BackButton(), // Tillbaka-knapp i appbaren
+        title: Text('TIG333 Att Göra'), // Titel på appbaren
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            // Textfält för att skriva in en ny uppgift
+            TextField(
+              controller: textController,
+              decoration: InputDecoration(
+                hintText: 'Vad ska du göra?', // Plats för input från användaren
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 20), // Tomt utrymme mellan textfältet och knappen
+            ElevatedButton(
+              onPressed: () {
+                // Här kan logik för att lägga till en ny uppgift implementeras
+              },
+              child: Text('+ LÄGG TILL'), // Text på knappen
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
